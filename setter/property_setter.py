97343617cd22ad19ad524c3ffa8c343c777c3feb
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-17 17:25:39 +0900

    add code snippets for property, setter decoration in python

---
+++ setter/property_setter.py
@@ -0,0 +1,23 @@
+class TestClass(object):
+    def __init__(self):
+        print("instance made.")
+        self.output = "this is origin output"
+    
+    def __str__(self):
+        return f'This instance id : {id(self)}'
+
+    @property
+    def stdout(self):
+        return self.output
+
+    @stdout.setter
+    def stdout(self, value):
+        print("setter called")
+        self.output = value
+
+if __name__ == "__main__":
+    tc = TestClass()
+    print(tc)
+    print(tc.stdout)
+    tc.stdout = "this is modified output" #setter를 calling하는 방법. tc.stdout()과 같이 사용하는 것이 아님.
+    print(tc.stdout)
