9653c41ac7627e2ccbeccb8fda7c100c74d24e52
Kimsehwan96 <sktpghks138@gmail.com>
2021-02-17 17:26:53 +0900

    add output

--- setter/property_setter.py
+++ setter/property_setter.py
@@ -21,3 +21,12 @@ if __name__ == "__main__":
     print(tc.stdout)
     tc.stdout = "this is modified output" #setter를 calling하는 방법. tc.stdout()과 같이 사용하는 것이 아님.
     print(tc.stdout)
+
+# Below Line is console output
+'''
+instance made.
+This instance id : 140397380905936
+this is origin output
+setter called
+this is modified output
+'''
